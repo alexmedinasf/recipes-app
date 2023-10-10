1257ed722cc4e814307c7bb607360f7f433aa85b
Alexandre <alexsales001@hotmail.com>
2023-10-10 18:33:53 -0300

    linters

--- app/models/recipe_food.rb
+++ app/models/recipe_food.rb
@@ -1,13 +1,13 @@
 class RecipeFood < ApplicationRecord
-    # associations
-    belongs_to :recipe, class_name: 'Recipe', foreign_key: 'recipe_id'
-    belongs_to :food, class_name: 'Food', foreign_key: 'food_id'
-  
-    # validations
-    validates :recipe_id, presence: true
-    validates :food_id, presence: true
-    validates :recipe_id, uniqueness: { scope: :food_id, message: "This food is already added to this recipe" }
-  
-    # validate quantity
-    validates :quantity, presence: true
-  end
+  # associations
+  belongs_to :recipe, class_name: 'Recipe', foreign_key: 'recipe_id'
+  belongs_to :food, class_name: 'Food', foreign_key: 'food_id'
+
+  # validations
+  validates :recipe_id, presence: true
+  validates :food_id, presence: true
+  validates :recipe_id, uniqueness: { scope: :food_id, message: 'This food is already added to this recipe' }
+
+  # validate quantity
+  validates :quantity, presence: true
+end
