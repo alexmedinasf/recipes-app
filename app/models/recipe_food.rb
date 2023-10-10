38456614f8d53ee445ca8a0d7e56a5183e2edb6d
Alexandre <alexsales001@hotmail.com>
2023-10-10 18:30:56 -0300

    added validation to models recipe and recipe_foods

--- app/models/recipe_food.rb
+++ app/models/recipe_food.rb
@@ -1,5 +1,13 @@
 class RecipeFood < ApplicationRecord
-  belongs_to :recipe
-
-  belongs_to :food
-end
+    # associations
+    belongs_to :recipe, class_name: 'Recipe', foreign_key: 'recipe_id'
+    belongs_to :food, class_name: 'Food', foreign_key: 'food_id'
+  
+    # validations
+    validates :recipe_id, presence: true
+    validates :food_id, presence: true
+    validates :recipe_id, uniqueness: { scope: :food_id, message: "This food is already added to this recipe" }
+  
+    # validate quantity
+    validates :quantity, presence: true
+  end
