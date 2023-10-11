1365c7c7d8a22f598549eb688a09146accbc21a1
Alexandre <alexsales001@hotmail.com>
2023-10-11 12:39:30 -0300

    fixed on devise lines

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'public_recipe#index'
-  devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
   # root "users#index"
   resources :recipe_foods
-  resources :users do
     resources :recipes do
       resources :foods
     end
