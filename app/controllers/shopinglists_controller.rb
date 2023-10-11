39915491ddb4ba00243d687fcef76b24e858b82e
Alexandre <alexsales001@hotmail.com>
2023-10-11 18:31:35 -0300

    rubocop

--- app/controllers/shopinglists_controller.rb
+++ app/controllers/shopinglists_controller.rb
@@ -1,5 +1,5 @@
 class ShopinglistsController < ApplicationController
-    def index
-      @recipefoods = RecipeFood.includes(:food).where(recipe_id: params[:recipe_id])
-    end
+  def index
+    @recipefoods = RecipeFood.includes(:food).where(recipe_id: params[:recipe_id])
   end
+end
