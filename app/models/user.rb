84f12fdc3441b3f361e0172c7497cb0bbc236a13
Alexandre <alexsales001@hotmail.com>
2023-10-11 12:10:55 -0300

    create  controller for the public recipes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
   has_many :foods, class_name: 'Food', foreign_key: 'user_id'
   has_many :recipes, class_name: 'Recipe', foreign_key: 'user_id'
 
   validates :name, presence: true
+
+  def recipes
+    recipes.order('created_at Desc')
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
+  root 'public_recipe#index'
+  devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
+  # root "users#index"
+  resources :recipe_foods
+  resources :users do
+    resources :recipes do
+      resources :foods
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :food, only: [:index, :show]
-  resources :recipes, only: [:inde, :show]
-
-  root 'foods#index'
 end
