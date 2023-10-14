2a32089cff63b741c29015ddbe57ab459adb23cf
HFG43 <hernanguemes@hotmail.com>
2023-10-13 23:35:58 -0300

    Feat: update shopping view & controller

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -1,7 +1,24 @@
 class ShoppingListsController < ApplicationController
   def index
-    current_user
+    @recipes = current_user.recipes.includes(:recipe_foods)
+    @value = total_value(@recipes)
+    @foods = current_user.foods
+  end
 
-    @recipefoods = RecipeFood.includes(:food).where(recipe_id: params[:recipe_id])
+  def total_value(recipes)
+    @food_data = {}
+    @total = 0
+    recipes.each do |recipe|      
+      recipe.recipe_foods.each do |rf|
+        if @food_data[rf.food.id].nil?
+          @food_data[rf.food.id] = rf.quantity
+        else
+          @food_data[rf.food.id] += rf.quantity
+        end
+        subtotal = ((rf.food.quantity - rf.quantity) * rf.food.price)
+        @total += subtotal      
+      end
+    end
+    [@total, @food_data]      
   end
 end
