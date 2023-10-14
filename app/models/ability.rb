53402f9cafb8573d130a4db2f9b2be456ecbadf5
HFG43 <hernanguemes@hotmail.com>
2023-10-13 21:41:06 -0300

    Feat: install Cancan gem & finish publi recipes view validations

---
+++ app/models/ability.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    can :read, Recipe, public: true
+  
+    return unless user.present?
+  
+    can :manage, Recipe, user_id: user.id
+  end
+end
+
